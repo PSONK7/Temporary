5c233950cde1a0251f46a3255bd0c58fa55c2108
PSONK7 <pratap@axonator.com>
2024-09-23 16:19:34 +0530

    "updated"

--- sort.py
+++ sort.py
@@ -11,11 +11,11 @@ def bubble_sort(arr):
 
 # Quicksort
 def quicksort(arr):
-    if len(arr) <= 1:
+    if len(arr) <= 1
         return arr
     pivot = arr[len(arr) // 2]
     left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
+    middle = [x for x in arr if x != pivot]
     right = [x for x in arr if x > pivot]
     return quicksort(left) + middle + quicksort(right)
 
@@ -26,5 +26,7 @@ if __name__ == "__main__":
     print("Original array:", arr)
     
     print("Bubble Sort:", bubble_sort(arr.copy()))
+
+    #remove comment later
 
     print("Quicksort:", quicksort(arr.copy()))

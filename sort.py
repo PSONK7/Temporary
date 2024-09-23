f68b69411c7223b62a940b9d58781f1ce7a9a850
PSONK7 <pratap@axonator.com>
2024-09-23 16:13:34 +0530

    "updated"

--- sort.py
+++ sort.py
@@ -1,3 +1,4 @@
+
 # Bubble Sort
 def bubble_sort(arr):
     n = len(arr)
@@ -7,27 +8,6 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
-# Selection Sort
-def selection_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        min_idx = i
-        for j in range(i+1, n):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-    return arr
-
-# Insertion Sort
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-    return arr
 
 # Quicksort
 def quicksort(arr):
@@ -46,6 +26,5 @@ if __name__ == "__main__":
     print("Original array:", arr)
     
     print("Bubble Sort:", bubble_sort(arr.copy()))
-    print("Selection Sort:", selection_sort(arr.copy()))
-    print("Insertion Sort:", insertion_sort(arr.copy()))
+
     print("Quicksort:", quicksort(arr.copy()))
